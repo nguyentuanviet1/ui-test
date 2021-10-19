e5647707e36a346a5dda4f32976f2bd5aa3dadd3
Viet Nguyen <viet.nguyentuan.ncc@gmail.com>
2021-10-19 21:52:53 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
 
 export default {
   name: 'App',
@@ -17,12 +22,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,61 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div >
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href=""><h2>Pluvego</h2></a>
+            <div class="absolute navbar-brand">Martin.D. <a href=""> Se déconnecter</a></div>
+        </nav>
+    </header>
+    <div class="">
+      <div class="row">
+        <div class="col-sm-2 content">
+          <div class="note-content">
+            <h4>Mes alertes</h4>
+            <h4>Mes assets</h4>
+            <h4>Mes contacts</h4>
+            <h4>Profil</h4>
+          </div>
+        </div>
+        <div class="col-sm-10">
+            <div class="a">
+            <h2>Profil</h2>
+            </div>
+            <div class="container note-content">
+              <label class="name"><b>Nom</b></label>
+              <input class="style-input" type="text" placeholder="Racine ">
+              <br>
+              <label class="name"><b>Prénom</b></label>
+              <input class="style-input" type="text" placeholder="Jean">
+              <br>
+              <label class="name"><b>Fonction</b></label>
+              <input class="style-input" type="text" placeholder="Directeur de la publication">
+              <br>
+              <label class="name"><b>Adresse email</b></label>
+              <input class="style-input" type="text" placeholder="jean.racine@societe.com" >
+              <br>
+              <div class="box">
+              <label class="name"><b>Numéro de téléphone</b></label>
+              <div class="input-group mb-3 dropdown-data">
+                <button type="button" class="btn btn-outline-secondary">Action</button>
+                <button type="button" class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                  <span class="visually-hidden">+30</span>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Separated link</a></li>
+                </ul>
+              </div>
+              <div class="phone-number">
+              <input type="text" placeholder="687654321" name="uname" value="">
+              </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,18 +70,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.a {
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.content {
+  height: 900px;
+ border-right: 1px solid black;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.note-content {
+  margin: 20px;
 }
-a {
-  color: #42b983;
+.name {
+  width: 180px;
+  margin-top: 30px;
+}
+.box {
+    display: flex;
+    flex-wrap: nowrap;
+}
+.dropdown-data {
+  margin-top: 20px;
+  width: 120px;
+}
+.phone-number {
+  margin-top: 20px;
+}
+.style-input {
+  width: 400px;
+}
+.absolute {
+  position: absolute;
+  right: 0;
+  /* color:; */
 }
 </style>
